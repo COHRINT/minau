458a1368afd2f1f9d1e28e9f94963b20e9af0de7
Luke Barbier <lukebarbier12@gmail.com>
2019-06-12 10:29:00 -0600

    More work to be done on bluerovs

--- scripts/point_planner.py
+++ scripts/point_planner.py
@@ -2,8 +2,11 @@
 from __future__ import division
 import rospy
 import random
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, Vector3
 import numpy as np
+from minau.srv import ArmControl, SetHeadingVelocity
+from nav_msgs.msg import Odometry
+import tf
 
 class Planner:
 
@@ -16,15 +19,35 @@ class Planner:
             self.twist = self.get_random_twist()
         else:
             self.twist = self.load_twist_dict(start_twist)
-        rospy.Timer(rospy.Duration(update_period), self.pub_cmd)
+        rospy.Subscriber('/' + name + '/pose_gt', Odometry, self.pose_callback)
+        rospy.wait_for_message('/' + name + '/pose_gt', Odometry)
+        rospy.Timer(rospy.Duration(update_period), self.update_auv)
         rospy.loginfo(name + " Planner Initialized.")
         self.seq = 0
+        if self.planner != 'stopped':
+            self.arm_uav()
+        else:
+            rospy.logwarn("Not arming: " + name)
+
+    def pose_callback(self, msg):
+        self.pose = msg.pose.pose
+
+    def arm_uav(self):
+        rospy.loginfo("Arming " + self.name)
+        srv_name = '/' + self.name + '/uuv_control/arm_control'
+        rospy.wait_for_service(srv_name)
+        req = rospy.ServiceProxy(srv_name, ArmControl)
+        try:
+            res = req()
+            rospy.logfatal(res)
+        except rospy.ServiceException as exc:
+            print("No response to " + srv_name)
         
     def load_twist_dict(self, twist_dict):
-        dot_x = twist_dict['x']
-        dot_y = twist_dict['y']
-        dot_z = twist_dict['z']
-        dot_psi = twist_dict['psi']
+        dot_x = float(twist_dict['x'])
+        dot_y = float(twist_dict['y'])
+        dot_z = float(twist_dict['z'])
+        dot_psi = float(twist_dict['psi'])
         twist = TwistStamped()
         twist.twist.linear.x = dot_x
         twist.twist.linear.y = dot_y
@@ -32,7 +55,6 @@ class Planner:
         twist.twist.angular.z = dot_psi
         return twist
 
-    
     def get_random_twist(self):
         [min_twist, max_twist] = rospy.get_param('/planners/random_linear_vel_range')
         [min_ang, max_ang] = rospy.get_param('/planners/random_angular_vel_range')
@@ -44,6 +66,44 @@ class Planner:
         size_ang = max_ang - min_ang
         twist.twist.angular.z = random.random() * size_ang + min_ang
         return twist
+
+    def set_hv(self, heading, velocity):
+        """ Sends a BlueROV along the desired velocity
+        
+        Parameters
+        ----------
+        heading : float
+        velocity : geometry_msgs/Vector3
+
+        Returns
+        -------
+        None
+        """
+        srv_name = '/' + self.name + '/uuv_control/set_heading_velocity'
+        rospy.wait_for_service(srv_name)
+        req = rospy.ServiceProxy(srv_name, SetHeadingVelocity)
+        try:
+            req(heading, velocity)
+        except rospy.ServiceException as exc:
+            print("No response to " + srv_name)
+
+    def update_auv(self, msg):
+        if self.planner == "stopped": # Nothing to do if we're stopped
+            return
+        
+        v = Vector3()
+        v.x = self.twist.twist.linear.x
+        v.y = self.twist.twist.linear.y
+        v.z = self.twist.twist.linear.z
+        
+        quat_list = [ self.pose.orientation.x, \
+                      self.pose.orientation.y, \
+                      self.pose.orientation.z, \
+                      self.pose.orientation.w ]
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quat_list)
+        heading = yaw + self.twist.twist.angular.z
+        self.set_hv(heading, v)
+        rospy.loginfo("Updating AUV")
         
     def pub_cmd(self, msg):
         new_twist = self.get_new_twist()
@@ -65,8 +125,10 @@ class Planner:
 def main():
     rospy.init_node('point_planner', anonymous=True)
     name = rospy.get_namespace().split('/')[1]
-    while not rospy.has_param('/planners') and not rospy.is_shutdown(): # wait for the param server to load
+    rospy.loginfo("waiting for params to become available")
+    while not rospy.has_param('/planners/update_freq') and not rospy.is_shutdown(): # wait for the param server to load
         pass
+    rospy.loginfo("params found")
     param_name = rospy.search_param('planners/update_freq')
     update_period = 1 / int(rospy.get_param(param_name))
     p = Planner(name, update_period)
